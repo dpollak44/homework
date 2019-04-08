bdf6e91aed64f6641db0b4f98ebece24e993cd72
Your Namedpollak44 <you@example.comdpollak44@gmail.com>
2019-04-07 21:24:29 -0400

    class 38

---
+++ python/37/hw1.py
@@ -0,0 +1,8 @@
+print("hello world")
+name= 'dovid pollak'
+address= '4 christina court jackson nj 08527'
+friends=['yossi','yanki','shmerel']
+print(f'name: {name},address: {address},friends: {friends}')
+print(name[2:12:3])
+best_friend=friends[-2]
+print(best_friend[1::2])
---
+++ python/38/hw 38.py
@@ -0,0 +1,15 @@
+import random 
+
+#for i in range (1,11):
+ #   for j in range (0,13): 
+  #      print (i*j)
+
+
+     
+compu_num=random.randint(1, 101)
+
+while x!=y:
+    guess = input('Pick a number from 1 to 100')
+if guess = compu_num:
+    x=y
+    print (you win)
