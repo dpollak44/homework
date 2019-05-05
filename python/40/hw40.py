3f13553a67d48afaab6ad3b71fef6b865706d58f
Your Namedpollak44 <you@example.comdpollak44@gmail.com>
2019-05-05 13:45:09 -0400

    hw

--- python/40/hw40.py
+++ python/40/hw40.py
@@ -1,13 +1,14 @@
-unsorted_list=[20,10,30,6,8,7]#i know it doesnt work.dont know why
-x=0
-for slot in unsorted_list:
-    if slot < unsorted_list[x]:
-            temp=slot
-            slot=unsorted_list[x]
-            unsorted_list[x]=temp
-            x+=1
-    else:
-            x+=1
-            print('a')
+list=[20,10,30,6,8,7]#i know it doesnt work.dont know why
+def sort(unsorted_list,x):
+    while x< len(unsorted_list)-1:
+            for slot in unsorted_list:
+                if slot < unsorted_list[x]:
+                    temp=slot
+                    slot=unsorted_list[x]
+                    unsorted_list[x]=temp
+                    x+=1
+            else:
+                x+=1
+            
 
-print(unsorted_list) 
+print(sort(list,0)) 
