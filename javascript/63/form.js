3848890c430242f20cc51894bf310e70327dfbbc
Your Namedpollak44 <you@example.comdpollak44@gmail.com>
2019-07-28 19:33:02 -0400

    hw

---
+++ javascript/63/form.js
@@ -0,0 +1,45 @@
+/*global $*/
+
+(function () {
+    'use strict';
+
+    const theTable = $('#contactsTable tbody');
+
+    const contacts = [];
+
+    function addContact(newContact) {
+        if (!contacts.length) {
+            theTable.empty();
+        }
+
+        contacts.push(newContact);
+
+        theTable.append(`<tr>
+                            <td>${newContact.firstName}</td>
+                            <td>${newContact.lastName}</td>
+                            <td>${newContact.address}</td>
+                         </tr>`);
+    }
+
+    const firstNameInput = $('#first');
+    const lastNameInput = $('#last');
+    const addresssInput = $('#address');
+    const contactForm = $('#contactForm');
+
+
+    contactForm.submit(event => {
+        if ($('#checked').checked) {
+            const newContact = {
+                firstName: firstNameInput.val(),
+                lastName: lastNameInput.val(),
+                addresssInput: addresssInput.val()
+            };
+
+            addContact(newContact);
+        }
+
+
+        event.preventDefault();
+    });
+
+}());
