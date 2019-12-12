eeedf53bc10911db96e79ec8490ccdeb5bc6dd88
david <david@dpollak44@gmail.com>
2019-12-11 19:48:03 -0500

    hw

--- javascript/stockAPI/my-stock-api/src/App.js
+++ javascript/stockAPI/my-stock-api/src/App.js
@@ -1,16 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        stock quotes
-      </header>
+class App extends Component {
 
-    </div>
-  );
+  state = {
+    // selectedStock: null
+  };
+
+  // handleStockSelected = stock => {
+  //   this.setState({
+  //     selectedStock: stock
+  //   });
+  // };
+
+
+
+  componentDidMount() {
+    fetch('https://api-v2.intrinio.com/companies?has_stock_prices=true&api_key=OmNkZWQyZGQxNDc5NWZjMTQ3NWI3NjJhZjljOThkMzQ2')
+
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to load');
+        }
+        return response.json();
+      })
+      .then(stocks => {
+        this.setState({
+          stocks: stocks
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  render() {
+    const { stocks } = this.state;
+    return (
+      <>
+        {stocks &&
+          stocks.map(stock =>
+            <div>{stock}</div>)}
+      </>
+    );
+    // <div>
+    //   {/* <Header/> */}
+    //   <hr />
+    //   <StockList onStockSelected={this.handleStockSelected} />
+    //   <hr />
+    //   {this.state.selectedStock && <Stock stock={this.state.selectedStock} />}
+    // </div>
+    // );
+  }
 }
 
 export default App;
